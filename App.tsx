b1ae0b83ae39e174a3a18b56ec85d922fb6f2732
shubham sarode <shubhamsarode435@gmail.com>
2025-02-21 21:37:24 +0530

    Update package.json for navigation dependencies and modify App component to use MainNavigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { NavigationContainer } from '@react-navigation/native';
-// import MainNavigation from './src/navigation/MainNavigation';
-import { Text, View } from 'react-native';
-import Signin from './src/screen/Signin';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from './src/navigation/MainNavigation';
+// import { Text, View } from 'react-native';
+// import Signin from './src/screen/Signin';
 const App = () => {
   return (
-    <>
-       <Signin /> 
-    </>
+    <NavigationContainer>
+       <MainNavigation /> 
+    </NavigationContainer>
   )
 }
 
