f9e0b6037925c4edc935408c011341ab309eaec5
shubham sarode <shubhamsarode435@gmail.com>
2025-02-22 21:00:35 +0530

    Update App test to render CommonButton component instead of App

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,10 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import CommonButton from '../src/components/common/CommonButton';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+    ReactTestRenderer.create(<CommonButton title='Sample test cases' />);
   });
 });
